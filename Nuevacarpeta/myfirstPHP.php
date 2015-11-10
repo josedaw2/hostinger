249897434ecc41f2db67c50e8ff04f624e30b2e2
josedaw2 <josedaw2@gmail.com>
2015-11-10 08:59:22 +0100

    mi primera aplicacion
    primera version

---
+++ Nuevacarpeta/myfirstPHP.php
@@ -0,0 +1,19 @@
+<!DOCTYPE HTML>
+<html>
+<head>
+<meta charset="utf-8">
+<title>Documento sin título</title>
+</head>
+
+<body>
+<?php
+$color = "red";
+echo "My car is " . $color . "<br>";
+
+
+echo '"aaaaaaa"';
+
+?>
+
+</body>
+</html>
